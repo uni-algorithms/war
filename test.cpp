c72e775053cf87190c4bbf268e3d8ff82a9aaaef
Luca Mosetti <luka.mosetti@gmail.com>
2018-12-06 17:56:59 +0100

    Retrieve info
    - Distances from stream
    - Advantages

--- test.cpp
+++ test.cpp
@@ -2,3 +2,32 @@
 
 #include <catch.hpp>
 #include "war.hpp"
+
+TEST_CASE("advantages") {
+    vector<int> dist_target_component = {5, 8};
+    vector<vector<int>> dist_soldiers_component = {{1, 2},
+                                                   {5, 9}};
+    vector<vector<int>> expected = {{-4, -6},
+                                    {0,  1}};
+    vector<vector<int>> actual;
+    advantages(dist_target_component, dist_soldiers_component, back_inserter(actual));
+    REQUIRE(expected == actual);
+}
+
+TEST_CASE("distances_from_stream") {
+    stringstream in("2 3\n"
+                    "2 0\n"
+                    "2 2\n"
+                    "5 5\n"
+                    "0 6");
+
+    vector<int> expected_tc = {5, 8};
+    vector<vector<int>> expected_ssc = {{1, 2},
+                                        {5, 8}};
+
+    vector<int> actual_tc;
+    vector<vector<int>> actual_ssc;
+    distances_from_stream(in, 2, 2, back_inserter(actual_tc), back_inserter(actual_ssc));
+    REQUIRE(expected_tc == actual_tc);
+    REQUIRE(expected_ssc == actual_ssc);
+}
--- war.hpp
+++ war.hpp
@@ -2,7 +2,66 @@
 #define WAR_HPP
 
 #include <fstream>
+#include <functional>
+#include <ostream>
+#include <vector>
 
 using namespace std;
+
+struct coordinates {
+    int x, y;
+};
+
+template<typename InputIterator, typename OutputIterator>
+void subtract(InputIterator a_first, InputIterator a_last, InputIterator b_first, OutputIterator out) {
+    for (; a_first != a_last; ++a_first, ++b_first) {
+        out++ = *a_first - *b_first;
+    }
+}
+
+template<typename OutputIterator>
+void pick_coordinates(istream &in, unsigned long n, OutputIterator out) {
+    for (auto i = 0; i < n; ++i) {
+        int x, y;
+        in >> x >> y;
+        out++ = {x, y};
+    }
+}
+
+template<typename OutputIterator>
+void advantages(const vector<int> &dist_tc, const vector<vector<int>> &dist_scs, OutputIterator adv) {
+    for (const auto &dist_sc : dist_scs) {
+        vector<int> sub;
+        subtract(begin(dist_sc), end(dist_sc), begin(dist_tc), back_inserter(sub));
+        adv++ = sub;
+    }
+}
+
+function<int(const coordinates)> distance(const coordinates &a) {
+    return [&](const coordinates &b) {
+        return abs(a.x - b.x) + abs(a.y - b.y);
+    };
+}
+
+template<typename OutputIteratorA, typename OutputIteratorB>
+void distances_from_stream(istream &in, unsigned long c, unsigned long s, OutputIteratorA dist_tc, OutputIteratorB dist_scs) {
+    vector<coordinates> components;
+    vector<coordinates> soldiers;
+
+    pick_coordinates(in, c, back_inserter(components));
+    pick_coordinates(in, s, back_inserter(soldiers));
+
+    int x, y;
+    in >> x >> y;
+    coordinates target = {x, y};
+
+    transform(begin(components), end(components), dist_tc, distance(target));
+
+    for (const auto &soldier : soldiers) {
+        vector<int> dist_sc;
+        transform(begin(components), end(components), back_inserter(dist_sc), distance(soldier));
+        dist_scs++ = dist_sc;
+    }
+}
 
 #endif //WAR_HPP
