d91063c776168917576f25875887d00674f90327
Luca Mosetti <luka.mosetti@gmail.com>
2018-12-08 19:55:46 +0100

    Implement Hungarian algorithm (Pilgrim version)

---
+++ hungarian.hpp
@@ -0,0 +1,241 @@
+#ifndef WAR_HUNGARIAN_HPP
+#define WAR_HUNGARIAN_HPP
+
+#include <iterator>
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <numeric>
+#include <ostream>
+#include <vector>
+#include <limits.h>
+#include <iostream>
+
+using namespace std;
+
+class node {
+    int r, c;
+
+public:
+    node() {}
+
+    node(int r, int c) : r(r), c(c) {}
+
+    int row() const { return r; }
+
+    int column() const { return c; }
+
+    bool operator==(const node &rhs) const {
+        return r == rhs.r &&
+               c == rhs.c;
+    }
+
+    bool operator!=(const node &rhs) const {
+        return !(rhs == *this);
+    }
+
+    bool operator<(const node &rhs) const {
+        if (r < rhs.r)
+            return true;
+        if (rhs.r < r)
+            return false;
+        return c < rhs.c;
+    }
+
+    bool operator>(const node &rhs) const {
+        return rhs < *this;
+    }
+
+    bool operator<=(const node &rhs) const {
+        return !(rhs < *this);
+    }
+
+    bool operator>=(const node &rhs) const {
+        return !(*this < rhs);
+    }
+
+    friend ostream &operator<<(ostream &os, const node &n) {
+        os << "[" << n.r << ", " << n.c << "]";
+        return os;
+    }
+};
+
+template<typename OutputIterator>
+void stars_unstars(const node &prime, vector<node> &primes, vector<node> &stars, OutputIterator to_stars,
+                   OutputIterator to_unstars) {
+
+    const auto star_it = find_if(begin(stars), end(stars),
+                                 [&prime](const node &star) { return prime.column() == star.column(); });
+
+    if (star_it == end(stars)) {
+        return;
+    }
+
+    const auto star = *star_it;
+    const auto to_star = *find_if(begin(primes), end(primes),
+                                  [&star](const node &prime) { return star.row() == prime.row(); });
+
+    to_stars++ = to_star;
+    to_unstars++ = star;
+
+    stars_unstars(to_star, primes, stars, to_stars, to_unstars);
+}
+
+void unstars(const node &prime, vector<node> &primes, vector<node> &stars, vector<bool> &cover_row) {
+
+    vector<node> to_stars = {prime};
+    vector<node> to_unstars;
+
+    stars_unstars(prime, primes, stars, back_inserter(to_stars), back_inserter(to_unstars));
+
+    if (to_unstars.size() == stars.size()) {
+        stars.clear();
+    }
+
+    if (!to_unstars.empty() && !stars.empty()) {
+        sort(to_unstars.begin(), to_unstars.end());
+        sort(stars.begin(), stars.end());
+
+        vector<node> diff;
+        set_difference(
+                begin(stars), end(stars),
+                begin(to_unstars), end(to_unstars),
+                back_inserter(diff)
+        );
+        stars = diff;
+    }
+
+
+    stars.insert(end(stars), begin(to_stars), end(to_stars));
+    primes.clear();
+    fill(begin(cover_row), end(cover_row), false);
+}
+
+bool
+find_primes(vector<vector<int>> &rows, unsigned long nr, unsigned long nc, vector<node> &stars, vector<node> &primes, vector<bool> &cover_col,
+            vector<bool> &cover_row) {
+
+    for (int r = 0; r < nr; ++r) {
+        for (int c = 0; c < nc; ++c) {
+            if (cover_row[r]) break;
+            if (cover_col[c]) continue;
+            if (rows[r][c] != 0) continue;
+
+            const node prime = {r, c};
+            primes.push_back(prime);
+            const auto star_it = find_if(begin(stars), end(stars), [&r](const node &star) { return r == star.row(); });
+
+            if (star_it == end(stars)) {
+                unstars(prime, primes, stars, cover_row);
+                return false;
+            }
+
+            cover_row[r] = true;
+            cover_col[star_it->column()] = false;
+        }
+    }
+
+    return true;
+}
+
+void minimize_rows(vector<vector<int>> &rows, unsigned long nc) {
+    for (auto &row : rows) {
+        int min = *min_element(begin(row), end(row));
+        transform(begin(row), end(row), begin(row), [&min](const int el) { return el - min; });
+    }
+}
+
+template<typename OutputIterator>
+void find_stars(vector<vector<int>> &rows, unsigned long nr, unsigned long nc, OutputIterator out) {
+
+    vector<bool> starred_row(nr, false);
+    vector<bool> starred_col(nc, false);
+
+    for (int r = 0; r < nr; ++r) {
+        for (int c = 0; c < nc; ++c) {
+            if (starred_row[r]) break;
+            if (starred_col[c]) continue;
+            if (rows[r][c] != 0) continue;
+
+            out++ = {r, c};
+            starred_row[r] = true;
+            starred_col[c] = true;
+        }
+    }
+}
+
+bool
+is_done(vector<vector<int>> &rows, unsigned long nr, unsigned long nc, vector<node> &stars, vector<bool> &cover_col) {
+
+    for (const auto &star : stars) {
+        cover_col[star.column()] = true;
+    }
+
+    long covered_col = count_if(begin(cover_col), end(cover_col), [](const bool b) { return b; });
+    return covered_col == nr;
+}
+
+void minimize_cross(vector<vector<int>> &rows, unsigned long nr, unsigned long nc, const vector<bool> &cover_col,
+                    const vector<bool> &cover_row) {
+    int minimum = INT_MAX;
+
+    for (int r = 0; r < nr; ++r) {
+        for (int c = 0; c < nc; ++c) {
+            if (cover_row[r]) break;
+            if (cover_col[c]) continue;
+
+            minimum = min(minimum, rows[r][c]);
+        }
+    }
+
+    for (int r = 0; r < nr; ++r) {
+        for (int c = 0; c < nc; ++c) {
+            if (!cover_row[r] && !cover_col[c]) {
+                rows[r][c] -= minimum;
+            }
+
+            if (cover_row[r] && cover_col[c]) {
+                rows[r][c] += minimum;
+            }
+        }
+    }
+}
+
+template<typename OutputIterator>
+void hungarian(vector<vector<int>> &rows, OutputIterator out) {
+    const auto nr = rows.size();
+    const auto nc = begin(rows)->size();
+
+    vector<bool> cover_row(nr, false);
+    vector<bool> cover_col(nc, false);
+    vector<node> stars;
+    vector<node> primes;
+
+    minimize_rows(rows, nc);
+    find_stars(rows, nr, nc, back_inserter(stars));
+
+    int step = 3;
+    while (step != 0) {
+        switch (step) {
+            case 3: {
+                const bool done = is_done(rows, nr, nc, stars, cover_col);
+                step = done ? 0 : 4;
+                break;
+            }
+            case 4: {
+                const bool which = find_primes(rows, nr, nc, stars, primes, cover_col, cover_row);
+                step = which ? 6 : 3;
+                break;
+            }
+            case 6: {
+                minimize_cross(rows, nr, nc, cover_col, cover_row);
+                step = 4;
+                break;
+            }
+        }
+    }
+
+    copy(begin(stars), end(stars), out);
+}
+
+#endif //WAR_HUNGARIAN_HPP
--- war.hpp
+++ war.hpp
@@ -1,18 +1,40 @@
 #ifndef WAR_HPP
 #define WAR_HPP
 
-#include <algorithm>
-#include <fstream>
-#include <functional>
-#include <numeric>
-#include <ostream>
-#include <vector>
+#include "hungarian.hpp"
 
 using namespace std;
 
 struct coordinates {
     int x, y;
 };
+
+void make_columns_great_again(vector<vector<int>> &rows, const int value) {
+    const auto n_r = rows.size();
+    const auto n_c = begin(rows)->size();
+
+    if (n_c < n_r) {
+        const auto diff = n_r - n_c;
+        for (auto &row : rows) {
+            fill_n(back_inserter(row), diff, value);
+        }
+    }
+}
+
+int make_positive(vector<vector<int>> &rows) {
+    int minimum = *begin(*begin(rows));
+    for (const auto &row : rows) {
+        minimum = min(minimum, *min_element(begin(row), end(row)));
+    }
+
+    if (minimum < 0) {
+        for (auto &row : rows) {
+            transform(begin(row), end(row), begin(row), [&minimum](const int &el) { return el - minimum; });
+        }
+    }
+
+    return minimum;
+}
 
 template<typename InputIterator, typename OutputIterator>
 void subtract(InputIterator a_first, InputIterator a_last, InputIterator b_first, OutputIterator out) {
@@ -30,16 +52,12 @@ void pick_coordinates(istream &in, unsigned long n, OutputIterator out) {
     }
 }
 
-int doubles(int acc, const int &x) {
-    return acc + x * 2;
-}
-
 template<typename OutputIterator>
 int one_soldier_solver(const vector<int> &dist_tc, const vector<vector<int>> &adv, OutputIterator who_what) {
-    transform(begin(dist_tc), end(dist_tc), who_what, [](const int ignore) { return 0; });
+    fill_n(who_what, dist_tc.size(), 0);
     const vector<int> dist_sc = *begin(adv);
     const auto min = *min_element(begin(dist_sc), end(dist_sc));
-    return accumulate(begin(dist_tc), end(dist_tc), min, doubles);
+    return accumulate(begin(dist_tc), end(dist_tc), 0) * 2 + min;
 }
 
 template<typename OutputIterator>
@@ -78,6 +96,19 @@ void distances_from_stream(istream &in, unsigned long c, unsigned long s, Output
     }
 }
 
+bool between(const long &x, const long &first, const long &last) {
+    return first <= x && x < last;
+}
+
+template<typename K, typename V>
+K take_first(const pair<K, V> &p) {
+    return p.first;
+}
+
+bool compare_columns(const node &a, const node &b) {
+    return a.column() < b.column();
+}
+
 template<typename OutputIterator>
 int min_time(istream &in, OutputIterator who_what) {
     unsigned long c;
@@ -94,8 +125,63 @@ int min_time(istream &in, OutputIterator who_what) {
         return one_soldier_solver(dist_tc, adv, who_what);
     }
 
-    // TODO
-    return -1;
+    vector<vector<int>> clone(adv);
+
+    const auto min_adv = make_positive(clone);
+
+    make_columns_great_again(clone, abs(min_adv));
+
+    vector<node> indexes;
+    hungarian(clone, back_inserter(indexes));
+
+    // sort indexes by component
+    sort(begin(indexes), end(indexes), compare_columns);
+
+    // remove non-existent components
+    indexes.resize(c);
+
+    // init minimums
+    int min_soldier = -1;
+    int min_chosen_adv = INT_MAX;
+
+    // find minimums
+    for (const auto &node : indexes) {
+        const auto soldier = node.row();
+        const auto component = node.column();
+
+        if (!between(soldier, 0, s)) continue;
+
+        const int candidate_adv = adv[soldier][component];
+
+        if (candidate_adv < min_chosen_adv) {
+            min_chosen_adv = candidate_adv;
+            min_soldier = soldier;
+        }
+    }
+
+    // total_adv is the sum of the advantages of the not useless soldier
+    int total_adv = 0;
+
+    vector<int> who_what_final;
+
+    // change every useless soldier to the min_soldier
+    // a soldier is useless <=> is not the min_soldier && (the adv >= 0 || does not belong to the matrix)
+    for (const auto &index : indexes) {
+        const auto soldier = index.row();
+        const auto component = index.column();
+
+        if (!between(soldier, 0, s) || adv[soldier][component] >= 0) {
+            who_what_final.push_back(min_soldier);
+            continue;
+        }
+
+        who_what_final.push_back(soldier);
+        total_adv += adv[soldier][component];
+    }
+
+    total_adv = min_chosen_adv >= 0 ? min_chosen_adv : total_adv;
+    copy(begin(who_what_final), end(who_what_final), who_what);
+    return accumulate(begin(dist_tc), end(dist_tc), 0) * 2 + total_adv;
 }
 
 #endif //WAR_HPP
